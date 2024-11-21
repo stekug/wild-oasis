588aa8f39eaccc85530b4c39e9c7e2c7894ce948
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2024-11-21 13:22:16 +0100

    build reusable Row component

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,35 @@ import GlobalStyles from './styles/GlobalStyles';
 import Button from './ui/Button';
 import Heading from './ui/Heading';
 import Input from './ui/Input';
+import Row from './ui/Row';
 
 export default function App() {
   return (
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">Wild Oasis App</Heading>
-        <Heading as="h2">Check in and out</Heading>
-        <Button onClick={() => alert('Check In')}>Check in</Button>
-        <Button onClick={() => alert('Check Out')}>Check out</Button>
-        <Heading as="h3">Form</Heading>
-        <Input type="number" placeholder="Number of guests" />
-        <Input type="number" placeholder="Number of guests" />
+        <Row type="vertical">
+          <Row type="horizontal">
+            <Heading as="h1">Wild Oasis App</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button onClick={() => alert('Check In')}>Check in</Button>
+              <Button onClick={() => alert('Check Out')}>Check out</Button>
+            </div>
+          </Row>
+          <Row type="vertical">
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
 }
 
 const StyledApp = styled.main`
-  background-color: orange;
   padding: 20px;
 `;
