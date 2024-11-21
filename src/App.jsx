29189896f1962a59ba83b3ecf61b86fa6068cef7
Stephan Kugler <150922232+stekug@users.noreply.github.com>
2024-11-21 13:29:09 +0100

    feat: Basic setup (#1)
    - add reusable header
    - build reusable Row component
    - build resuable button

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,49 @@
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Heading from './ui/Heading';
+import Input from './ui/Input';
+import Row from './ui/Row';
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row type="vertical">
+          <Row type="horizontal">
+            <Heading as="h1">Wild Oasis App</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button
+                variation="primary"
+                size="medium"
+                onClick={() => alert('Check In')}
+              >
+                Check in
+              </Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert('Check Out')}
+              >
+                Check out
+              </Button>
+            </div>
+          </Row>
+          <Row type="vertical">
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
+  );
 }
+
+const StyledApp = styled.main`
+  padding: 20px;
+`;
