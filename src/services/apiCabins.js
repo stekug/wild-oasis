c470462051d9a91dfbdbd8522c3fff26ad578b7f
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2024-11-24 18:54:09 +0100

    add delete logic in CabinRow

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -14,7 +14,7 @@ export async function deleteCabin(id) {
   const { data, error } = await supabase.from('cabins').delete().eq('id', id);
   if (error) {
     console.error(error);
-    throw new Error('Cabins could not be deleted');
+    throw new Error('Cabin could not be deleted');
   }
   return data;
 }
