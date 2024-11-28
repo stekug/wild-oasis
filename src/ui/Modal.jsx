b1685b691128337264c5ffd010b7e64bcb2ac280
Stephan Kugler <150922232+stekug@users.noreply.github.com>
2024-11-28 20:56:04 +0100

    feat: Modal window (#14)
    - add modal logic and AddCabin component to render Modal
    - use a portal to place the Modal as a direct child of the body

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,4 +1,6 @@
-import styled from "styled-components";
+import { createPortal } from 'react-dom';
+import { HiXMark } from 'react-icons/hi2';
+import styled from 'styled-components';
 
 const StyledModal = styled.div`
   position: fixed;
@@ -48,3 +50,17 @@ const Button = styled.button`
     color: var(--color-grey-500);
   }
 `;
+
+export default function Modal({ children, onCloseModal }) {
+  return createPortal(
+    <Overlay>
+      <StyledModal>
+        <Button onClick={onCloseModal}>
+          <HiXMark />
+        </Button>
+        <div>{children}</div>
+      </StyledModal>
+    </Overlay>,
+    document.body
+  );
+}
