f1dc63cee6d765df22d6b0266c61aef3e9638913
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2024-11-29 11:25:09 +0100

    refactor(Modal): refactor Modal and use Compound Component Pattern

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,3 +1,4 @@
+import { cloneElement, createContext, useContext, useState } from 'react';
 import { createPortal } from 'react-dom';
 import { HiXMark } from 'react-icons/hi2';
 import styled from 'styled-components';
@@ -51,11 +52,35 @@ const Button = styled.button`
   }
 `;
 
-export default function Modal({ children, onCloseModal }) {
+const ModalContext = createContext();
+
+function Modal({ children }) {
+  const [openName, setOpenName] = useState('');
+
+  const close = () => setOpenName('');
+  const open = setOpenName;
+
+  return (
+    <ModalContext.Provider value={{ openName, close, open }}>
+      {children}
+    </ModalContext.Provider>
+  );
+}
+
+function Open({ children, opens: opensWindowName }) {
+  const { open } = useContext(ModalContext);
+
+  return cloneElement(children, { onClick: () => open(opensWindowName) });
+}
+
+function Window({ children, name }) {
+  const { openName, close } = useContext(ModalContext);
+  if (name !== openName) return null;
+
   return createPortal(
     <Overlay>
       <StyledModal>
-        <Button onClick={onCloseModal}>
+        <Button onClick={close}>
           <HiXMark />
         </Button>
         <div>{children}</div>
@@ -64,3 +89,8 @@ export default function Modal({ children, onCloseModal }) {
     document.body
   );
 }
+
+export default Modal;
+
+Modal.Open = Open;
+Modal.Window = Window;
