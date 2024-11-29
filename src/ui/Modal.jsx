a7902edf0898ebbec3064c71e75054628100bf82
Stephan Kugler <150922232+stekug@users.noreply.github.com>
2024-11-29 11:52:41 +0100

    refactor: Modal (#15)
    - refactor(Modal): refactor Modal and use Compound Component Pattern
    - fix Modal style
    - add global event listener to Modal to close Modal when clicking outside
    - refactor(Modal): create customHook to handle outside click

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,6 +1,15 @@
+import {
+  cloneElement,
+  createContext,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import { createPortal } from 'react-dom';
 import { HiXMark } from 'react-icons/hi2';
 import styled from 'styled-components';
+import { useOutsideClick } from '../hooks/useOutsideClick';
 
 const StyledModal = styled.div`
   position: fixed;
@@ -51,16 +60,47 @@ const Button = styled.button`
   }
 `;
 
-export default function Modal({ children, onCloseModal }) {
+const ModalContext = createContext();
+
+function Modal({ children }) {
+  const [openName, setOpenName] = useState('');
+
+  const close = () => setOpenName('');
+  const open = setOpenName;
+
+  return (
+    <ModalContext.Provider value={{ openName, close, open }}>
+      {children}
+    </ModalContext.Provider>
+  );
+}
+
+function Open({ children, opens: opensWindowName }) {
+  const { open } = useContext(ModalContext);
+
+  return cloneElement(children, { onClick: () => open(opensWindowName) });
+}
+
+function Window({ children, name }) {
+  const { openName, close } = useContext(ModalContext);
+  const ref = useOutsideClick(close);
+
+  if (name !== openName) return null;
+
   return createPortal(
     <Overlay>
-      <StyledModal>
-        <Button onClick={onCloseModal}>
+      <StyledModal ref={ref}>
+        <Button onClick={close}>
           <HiXMark />
         </Button>
-        <div>{children}</div>
+        <div>{cloneElement(children, { onCloseModal: close })}</div>
       </StyledModal>
     </Overlay>,
     document.body
   );
 }
+
+export default Modal;
+
+Modal.Open = Open;
+Modal.Window = Window;
