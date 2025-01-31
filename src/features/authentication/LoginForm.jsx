d923a1898f8a85babf39a8950edde0fdaadb10b4
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2025-01-31 16:01:45 +0100

    add temporal login data and login API

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -3,12 +3,19 @@ import Button from '../../ui/Button';
 import Form from '../../ui/Form';
 import Input from '../../ui/Input';
 import FormRowVertical from '../../ui/FormRowVertical';
+import { useLogin } from './useLogin';
+import SpinnerMini from '../../ui/SpinnerMini';
 
 function LoginForm() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('stephan@example.com');
+  const [password, setPassword] = useState('pass12345');
+  const { login, isLoggingIn } = useLogin();
 
-  function handleSubmit() {}
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (!email || !password) return;
+    login({ email, password });
+  }
 
   return (
     <Form onSubmit={handleSubmit}>
@@ -20,6 +27,7 @@ function LoginForm() {
           autoComplete="username"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          disabled={isLoggingIn}
         />
       </FormRowVertical>
       <FormRowVertical label="Password">
@@ -29,10 +37,13 @@ function LoginForm() {
           autoComplete="current-password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          disabled={isLoggingIn}
         />
       </FormRowVertical>
       <FormRowVertical>
-        <Button size="large">Login</Button>
+        <Button size="large" disabled={isLoggingIn}>
+          {!isLoggingIn ? 'Log in' : <SpinnerMini />}
+        </Button>
       </FormRowVertical>
     </Form>
   );
