9d1c62d5b8e1ae219f53095e694f1111b4f43d60
Stephan Kugler (MAC) <stephankugler.stuff@gmail.com>
2025-02-02 18:43:52 +0100

    add logout and spinner to logout button

--- src/features/authentication/Logout.jsx
+++ src/features/authentication/Logout.jsx
@@ -1,10 +1,14 @@
 import { HiArrowRightOnRectangle } from 'react-icons/hi2';
 import ButtonIcon from '../../ui/ButtonIcon';
+import { useLogout } from './useLogout';
+import SpinnerMini from '../../ui/SpinnerMini';
 
 export default function Logout() {
+  const { logout, isLoading } = useLogout();
+
   return (
-    <ButtonIcon>
-      <HiArrowRightOnRectangle />
+    <ButtonIcon disabled={isLoading} onClick={logout}>
+      {!isLoading ? <HiArrowRightOnRectangle /> : <SpinnerMini />}
     </ButtonIcon>
   );
 }
