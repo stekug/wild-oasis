79a04cd7c0d55256775e961103106fc52abf255d
Stephan Kugler <stephan.kugler@adesso.de>
2025-02-10 14:17:57 +0100

    compute recent bookings and stays

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from 'styled-components';
+import { useRecentBookings } from './useRecentBookings';
+import Spinner from '../../ui/Spinner';
+import { useRecentStays } from './useRecentStays';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -8,6 +11,15 @@ const StyledDashboardLayout = styled.div`
 `;
 
 export default function DashboardLayout() {
+  const { bookings, isLoading: isLoadingRecentBookings } = useRecentBookings();
+  const {
+    stays,
+    confirmedStays,
+    isLoading: isLoadingRecentStays,
+  } = useRecentStays();
+
+  if (isLoadingRecentBookings || isLoadingRecentStays) return <Spinner />;
+
   return (
     <StyledDashboardLayout>
       <div>Statistics</div>
